43a963dd7f58c681dbc25302759979821e2c713b
aengun <aengun@icloud.com>
2020-12-02 18:24:58 +0900

    node 선택

---
+++ 201202practice/index.js
@@ -0,0 +1,35 @@
+window.addEventListener("load", function(){
+    var s = document.querySelector("#s2");
+    var container = s.querySelector(".container");
+    var addButton = s.querySelector(".add-button");
+    var delButton = s.querySelector(".del-button");
+    var repButton = s.querySelector(".rep-button");
+
+    var addInText = s.querySelector(".add-in-text");
+    var oldRep = s.querySelector(".old-rep");
+    var newRep = s.querySelector(".new-rep");
+
+    var index = 1;
+
+    addButton.onclick = function () {
+        var div = document.createElement("div");
+        var span = document.createElement("span");
+        span.append(addInText.value+index++);
+        div.append(span);
+        container.append(div);
+    };
+
+    delButton.onclick = function () {
+        container.lastElementChild.remove();
+    };
+
+    repButton.onclick = function () {
+        var num1 = oldRep.value;
+        var num2 = newRep.value;
+        var oldOne = container.children[num1];
+        var newOne = container.children[num2];
+        container.replaceChild(newOne, oldOne); 
+        container.insertBefore(oldOne, newOne);
+    };
+
+});
